fd0b52947d4c1199094c79fc4ee9d9aed7d0793d
Wilterson Garcia <wilterson@peepi.com.br>
2021-07-25 21:16:29 -0300

    Added Tests TDD

--- test/api.test.js
+++ test/api.test.js
@@ -1,15 +1,34 @@
 const request = require('supertest');
 
-const app = require('../src/app');
+const shipment = require('./shipment.json');
+const shipments = require('./shipment.json');
+const payload = require('./payload.json');
 
-describe('GET /api/v1', () => {
-  it('responds with a json message', (done) => {
+const app = require('../src/server');
+
+describe('GET /api/v1/shipments/1', () => {
+  it('get a list of shipments', (done) => {
     request(app)
       .get('/api/v1/shipments')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
-      .expect(200, {
-        message: ''
-      }, done);
+      .expect(200, shipments, done);
+  });
+
+  it('get a specific shipment', (done) => {
+    request(app)
+      .get('/api/v1/shipments')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, shipment, done);
+  });
+
+  it('saves a new shipment', (done) => {
+    request(app)
+      .post('/api/v1/shipments')
+      .send(payload)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, payload, done);
   });
 });
