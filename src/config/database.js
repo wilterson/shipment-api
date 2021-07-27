cff202cada159342c50a1af43af02e68ffdc2edf
Wilterson Garcia <wilterson@peepi.com.br>
2021-07-26 21:35:54 -0300

    removed credentials

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'ec2-34-204-128-77.compute-1.amazonaws.com',
-  username: 'slfdqjejwtgpkl',
-  password: 'a4b1760bb0f070c4006bf9a0d1b99b681c0889c959cedc066a53d1a2ca59f190',
-  database: 'd14o7jlbllv8gr',
+  host: process.env.PGHOST || '127.0.0.1',
+  username: process.env.PGUSERNAME || 'postgres',
+  password: process.env.PGPASSWORD || 'docker',
+  database: process.env.PGDATABASE || 'shipment_api',
   define: {
     timestamps: true,
     underscored: true,
