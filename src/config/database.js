2222d4ba3221f2ceed16f96fb920d75cec08db76
Wilterson Garcia <wilterson@peepi.com.br>
2021-07-26 21:01:00 -0300

    added prod credentials

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'postgres',
-  host: process.env.PGHOST,
-  username: 'postgres',
-  password: 'docker',
-  database: 'shipment_api',
+  host: 'ec2-34-204-128-77.compute-1.amazonaws.com',
+  username: 'slfdqjejwtgpkl',
+  password: 'a4b1760bb0f070c4006bf9a0d1b99b681c0889c959cedc066a53d1a2ca59f190',
+  database: 'd14o7jlbllv8gr',
   define: {
     timestamps: true,
     underscored: true,
